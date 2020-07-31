db7ba9915d95f8bc0f74a2530280c2cf26f4d255
AbHi <55599278+slim-python@users.noreply.github.com>
2020-07-31 23:12:15 +0530

    Create var.js

---
+++ var.js
@@ -0,0 +1,12 @@
+var income = 550;
+console.log(income);
+//output : 550
+
+var income = 560;
+console.log(income);
+//output : 560
+
+var income = income + 1;
+console.log(income);
+//output : 561
+
